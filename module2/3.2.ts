36e4decc5b210641ce87390de8246cfd98b4dd9a
Md. Shahjalal Sheikh Bin Karsk <muhommodshahjalal@gmail.com>
2024-11-03 10:07:16 +0600

    added

--- module2/3.2.ts
+++ module2/3.2.ts
@@ -30,20 +30,19 @@
   type Roll1 = number[];
 
   interface Roll2 {
-   [index : number ] : number
+    [index: number]: number;
   }
 
-  const rollNumber1: Roll2 = [1,2,3]
-                              0 1 2 --> number type
+  const rollNumber1: Roll2 = [1, 2, 3];
+  //---------------------  // 0 1 2 --> number type
 
+  type Add1 = (num1: number, num2: number) => number;
 
-  type Add1 = (num1: number,num2:number)=> number
-  
   interface Add2 {
-    (num1: number,num2:number) : number
+    (num1: number, num2: number): number;
   }
 
-  const add: Add2 = (num1 , num2 )=> num1+num2
-  
+  const add: Add2 = (num1, num2) => num1 + num2;
+
   //
 }
